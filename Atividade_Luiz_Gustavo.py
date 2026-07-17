2dcb6c0f4bd52d099f8128931b87e2f2f72536e4
LuizDK10 <luiz.gustavo.tec2025@gmail.com>
2026-07-17 17:12:42 -0300

    Update Atividade_Luiz_Gustavo.py

--- Atividade_Luiz_Gustavo.py
+++ Atividade_Luiz_Gustavo.py
@@ -17,7 +17,7 @@ while True:
             arquivo.write(f"{titulo};{autor};Disponível\n")
         print("Livro cadastrado com sucesso!")
 
-    if r == 2:
+    elif r == 2:
         with open("livros.txt", "r", encoding='utf-8') as arquivo:
             linhas = arquivo.readlines()
             qtd = 0
@@ -27,8 +27,73 @@ while True:
             if qtd == 0:
                 print("Nenhum Livro encontrado.")
             else:
-                for linhas in linha:
+                for linha in linhas:
                     dados = linha.split(";")
                     print("Título:", dados[0])
                     print("Autor:", dados[1])
                     print("Status:", dados[2])
+
+    elif r == 3:
+        with open("livros.txt", "r", encoding='utf-8') as arquivo:
+            linhas = arquivo.readlines()
+            qtd = 0
+
+            for linha in linhas:
+                dados = linha.split(";")
+                if dados[2].strip() == "Disponível":
+                    qtd += 1
+                    print("Título:", dados[0])
+                    print("Autor:", dados[1])
+                    print("Status:", dados[2])
+            if qtd == 0:
+                print("Nenhum livro disponível.")
+    elif r == 4:
+        nome = input("Digite o nome do livro: ")
+        with open("livros.txt", "r", encoding="utf-8") as arquivo:
+            linhas = arquivo.readlines()
+        qtd = 0
+
+        with open("livros.txt", "w", encoding="utf-8") as arquivo:
+            for linha in linhas:
+                dados = linha.split(";")
+                if dados[0] == nome:
+                    qtd += 1
+                    if dados[2].strip() == "Disponível":
+                        arquivo.write(dados[0] + ";" + dados[1] + ";Emprestado\n")
+                        print("Livro emprestado!")
+                    else:
+                        arquivo.write(linha)
+                        print("Esse livro já está emprestado.")
+                else:
+                    arquivo.write(linha)
+        if qtd == 0:
+            print("Livro não encontrado.")
+
+    if r == 5:
+        nome = input("Digite o nome do livro: ")
+        with open("livros.txt", "r", encoding="utf-8") as arquivo:
+            linhas = arquivo.readlines()
+        qtd = 0
+
+        with open("livros.txt", "w", encoding="utf-8") as arquivo:
+            for linha in linhas:
+                dados = linha.split(";")
+
+                if dados[0] == nome:
+                    qtd += 1
+
+                    if dados[2].strip() == "Emprestado":
+                        arquivo.write(dados[0] + ";" + dados[1] + ";Disponível\n")
+                        print("Livro devolvido!")
+                    else:
+                        arquivo.write(linha)
+                        print("Esse livro já está disponível.")
+                else:
+                    arquivo.write(linha)
+
+        if qtd == 0:
+            print("Livro não encontrado.")
+
+    elif r == 6:
+        print("Encerrando o programa...")
+        break
