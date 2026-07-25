0c5be8f2a961c1680b80b1145fc874fc28269dad
LuizDK10 <luiz.gustavo.tec2025@gmail.com>
2026-07-24 21:20:05 -0300

    Delete atividade2_Luiz_Gustavo.py

--- atividade2_Luiz_Gustavo.py
+++
@@ -1,9 +0,0 @@
-with open("numeros.txt", "r") as arquivo:
-    linhas = []
-    for i in arquivo:
-        linhas.append(int(i))
-        linhas.sort()
-
-with open("numeros_organizados.txt", "w") as f:
-    for item in linhas:
-        f.write(str(item) + '\n')
