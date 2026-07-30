4aa86f4f1e62712048c4d3eebcda1143a995f4f1
LuizDK10 <luiz.gustavo.tec2025@gmail.com>
2026-07-30 20:32:18 -0300

    Update Atividade3_Luiz_Gustavo.py

--- Luiz Gustavo/Atividade3_Luiz_Gustavo.py
+++ Luiz Gustavo/Atividade3_Luiz_Gustavo.py
@@ -1,6 +1,7 @@
-with open ("neymar.txt", "r", encoding = 'utf-8') as arquivo:
-    lista = []
-    linhas = arquivo.readlines()
-    for i in linhas:
-        dados = i.split(' ')
-                
+with open("texto.txt", "r", encoding="utf-8") as arquivo:
+    texto = arquivo.read()
+texto = texto.replace("NEYMAR ", "")
+texto = texto.replace(" NEYMAR", "")
+
+with open("texto_corrigido.txt", "w", encoding="utf-8") as arquivo:
+    arquivo.write(texto)
